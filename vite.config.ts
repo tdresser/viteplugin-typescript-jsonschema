c89053844da02c8d71a90676c79dbcaa2736bec8
Tim Dresser <tdresser@google.com>
2024-12-16 12:10:28 -0500

    Switch to general transform

--- vite.config.ts
+++ vite.config.ts
@@ -10,26 +10,26 @@ const buildJSONSchemasPlugin = () => {
   const schemaGenerator = TJS.buildGenerator(program)
   return {
     name: 'build-json-schemas-plugin',
-    //transform(src: string, id: string) {
-    //      if (/\.html$/.test(id)) {
-    transformIndexHtml(html) {
-      const toolRegistrationStr = html.match(declarativeSchemaRegex)![1]
-      const toolRegistration = JSON.parse(toolRegistrationStr)
-      const schema = schemaGenerator?.getSchemaForSymbol(
-        toolRegistration.params
-      )
-      toolRegistration.params = schema
-      const substituted = html.replace(
-        declarativeSchemaRegex,
-        `<script type="tool-registration">${JSON.stringify(
-          toolRegistration,
-          null,
-          2
-        )}</script>`
-      )
-      return substituted
+    transform(html: string, id: string) {
+      if (/\.html$/.test(id)) {
+        //transformIndexHtml(html) {
+        const toolRegistrationStr = html.match(declarativeSchemaRegex)![1]
+        const toolRegistration = JSON.parse(toolRegistrationStr)
+        const schema = schemaGenerator?.getSchemaForSymbol(
+          toolRegistration.params
+        )
+        toolRegistration.params = schema
+        const substituted = html.replace(
+          declarativeSchemaRegex,
+          `<script type="tool-registration">${JSON.stringify(
+            toolRegistration,
+            null,
+            2
+          )}</script>`
+        )
+        return substituted
+      }
     },
-    //},
   }
 }
 
